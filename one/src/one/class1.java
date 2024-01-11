61b91d51835431ff66e55ebf0c94486a52c51625
home_3600 <home_3600@DESKTOP-PARCR2F>
2024-01-11 22:59:20 +0900

    22시57분commit테스트

---
+++ one/src/one/class1.java
@@ -0,0 +1,12 @@
+package one;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	}
+
+}
